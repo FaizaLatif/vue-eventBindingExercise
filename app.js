1f0d1bf2d2f4d65a94d31e904baa7e8594922d5e
faiza latif <faiza@outlook.pt>
2021-10-01 12:02:45 +0100

    exercise 2

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 Vue.createApp({
     data() {
         return {
-
+            exercise2: ''
         }
     },
     methods: {
         alert() {
             alert('button clicked');
+        },
+        setValue(event) {
+            this.exercise2 = event.target.value;
         }
     }
 }).mount('#assignment');
